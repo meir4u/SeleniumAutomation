2971ee99701816a5b32403b00a280cf58c55fe52
Meir Achildiev <meir.shopbroker@gmail.com>
2024-07-17 00:56:42 +0300

    added selenium automation and client functionality

--- src/client/show-ul.js
+++ src/client/show-ul.js
@@ -1,1 +1,15 @@
-﻿
+﻿let jsonData;
+
+jsonData = await fetch('http://localhost:59436/data/site-data.json')
+                .then((response) => response.json());
+
+console.log(jsonData);
+
+let ulElement = document.getElementsByTagName("ul")[0];
+
+for (let key in jsonData) {
+    var node = document.createElement("li");
+    var content = document.createTextNode(key);
+    node.appendChild(content);
+    ulElement.appendChild(node);
+}
--- src/selenium-automation/SeleniumAutomationTests/Program.cs
+++ src/selenium-automation/SeleniumAutomationTests/Program.cs
@@ -1,19 +1,54 @@
-﻿using OpenQA.Selenium;
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SeleniumAutomationTests
 {
     internal class Program
     {
+        private static readonly string url = "https://mdn.github.io/learning-area/html/tables/assessment-finished/planets-data.html";
+        private static readonly string fileLocation = "..\\..\\..\\..\\..\\client\\data\\site-data.json";
+
         static void Main(string[] args)
         {
-            var url = "";
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("");
-            driver.FindElement(By.XPath("//*[@id='editor']"));
+            var dictionaryData = new Dictionary<string, int>();
+            var dictionaryDataOrdered = new Dictionary<string, int>();            
 
+            using var driver = new ChromeDriver();
+
+            
+            driver.Navigate().GoToUrl(url);
+
+            var elements = driver.FindElements(By.XPath("//tbody/tr"));
+
+            foreach ( var element in elements )
+            {
+                var name = element.FindElement(By.XPath("./th[@scope='row' and not(@colspan)]"));
+                var diameter = element.FindElement(By.XPath("./td[2]"));
+
+
+                try
+                {
+                    dictionaryData.Add(name.Text, (int)double.Parse(diameter.Text));
+
+                }catch( Exception ex )
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            driver.Close();
+
+            dictionaryDataOrdered = dictionaryData.OrderByDescending( d => d.Value ).ToDictionary(x=>x.Key, x => x.Value);
+
+            var jsonData = JsonConvert.SerializeObject(dictionaryDataOrdered);
+            
+            System.IO.File.WriteAllText(fileLocation, jsonData.ToString());
 
             Console.WriteLine("Application End");
+
+
         }
     }
 }
