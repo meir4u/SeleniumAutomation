801e9ffaecefdc2116def7a4729f6855cc377577
Meir Achildiev <meir.shopbroker@gmail.com>
2024-07-16 14:28:11 +0300

    Add project files.

---
+++ src/selenium-automation/SeleniumAutomationTests/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumAutomationTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "";
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("");
+            driver.FindElement(By.XPath("//*[@id='editor']"));
+
+
+            Console.WriteLine("Application End");
+        }
+    }
+}
